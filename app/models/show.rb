a42fcce3c84a91371606be5e5d964135fae2ed93
Pauline Muigai <wanjirumuigai@gmail.com>
2023-03-29 13:41:04 +0300

    completed lab

---
+++ app/models/show.rb
@@ -0,0 +1,27 @@
+class Show < ActiveRecord::Base
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.all.map { |s| s if s.rating == self.highest_rating }.compact.first
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.all.map { |s| s if s.rating == self.lowest_rating }.compact.first
+  end
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+  def self.popular_shows
+    Show.all.map { |s| s if s.rating > 5 }.compact
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order(name: :asc)
+  end
+end
